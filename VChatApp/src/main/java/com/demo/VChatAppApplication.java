eca6f48c8606079824db14dab568a2d022957802
VIET LE <vietle211@gmail.com>
2021-08-17 11:06:45 -0400

    Upload App

---
+++ VChatApp/src/main/java/com/demo/VChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VChatAppApplication.class, args);
+	}
+
+}
